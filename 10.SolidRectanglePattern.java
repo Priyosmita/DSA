8a73585cf93ac55f2c1d0b20b48987e634fc4bb1
Priyos <156951756+Priyosmita@users.noreply.github.com>
2025-02-07 01:31:12 +0530

    Create 10.SolidRectanglePattern.java

---
+++ 10.SolidRectanglePattern.java
@@ -0,0 +1,25 @@
+/* Taken from Apna College Java Placement Course video #5
+*****
+*****
+*****
+*****
+*/
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height: ");
+        int h = sc.nextInt();
+        System.out.println("Enter the width: ");
+        int w = sc.nextInt();
+        for(int i=0;i<h;i++)
+        {
+            for(int j=0;j<w;j++)
+            {
+                 System.out.print("*");
+            }
+             System.out.println();
+        }
+    }
+}
