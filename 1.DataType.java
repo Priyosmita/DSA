badb1138ddae1c6753403ad4ac148fd80925b900
Priyos <156951756+Priyosmita@users.noreply.github.com>
2025-02-10 23:49:20 +0530

    Update 1.DataType.java

--- 1.DataType.java
+++ 1.DataType.java
@@ -1,3 +1,4 @@
+
 /*https://www.geeksforgeeks.org/problems/data-type-1666706751/0
 Geek is learning a new programming language. As data type forms the most fundamental part of a language, Geek is learning them with focus and needs your help.
 Given a data type, help Geek in finding the size of it in byte.
