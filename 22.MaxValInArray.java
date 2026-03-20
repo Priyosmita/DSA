2793c1a9104deb416fa21e0c8af6569171a2d7bd
Priyos <156951756+Priyosmita@users.noreply.github.com>
2026-03-20 21:48:03 +0530

    Create 22.MaxValInArray.java

---
+++ 22.MaxValInArray.java
@@ -0,0 +1,30 @@
+// Practicing from Kunal Kushwal=ha's videos
+
+ import java.util.*;
+ 
+ public class Main{
+    public static void main(String[] args) {
+         
+        Scanner sc= new Scanner (System.in);
+         
+        ArrayList <Integer> list = new ArrayList<Integer>(5);
+        
+        System.out.println("Enter the array:"); 
+        for(int i=0;i<5;i++) {
+            list.add(sc.nextInt());
+        }
+        
+        System.out.println("Max element is:" +max(list));
+    }
+    
+    public static int max(ArrayList <Integer> list) {
+        int m= list.get(0);
+        for(int i=0; i<list.size();i++) {
+            m= m<list.get(i)? m= list.get(i):m;
+        }
+        
+        return (m);
+    }
+ }
+
+// note to self: Can also be done if a certain range is given, then simply pass the range to the max funtion and iterate only within the range
